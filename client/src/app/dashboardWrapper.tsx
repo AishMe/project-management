92142d062a9d8fa586294a570dd64014f0f918f3
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2025-06-11 23:01:11 +0530

    add global variables

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
 import Sidebar from './Components/Sidebar'
+import StoreProvider, { useAppSelector } from './redux'
 
-const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+const DashboardLayout = ({ children }: {children: React.ReactNode}) => {
+  const isSidebarClosed = useAppSelector((state) => state.global.isSidebarClosed)
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  })
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
         < Sidebar />
-        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarClosed ? "" : "md:pl-64"}`}>
             < Navbar />
             {children}
         </main>
     </div>
+  )
+}
 
+const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   )
 }
 
